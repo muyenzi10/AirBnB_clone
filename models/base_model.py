da3a6f3cfcb405d3e05778c8dd8f650efdcadf81
muyenzi10 <muyenzija10@gmail.com>
2024-01-15 04:28:12 -0800

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,20 @@
 from datetime import datetime
 import uuid
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        t_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
+
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == "__class__":
+                    continue
+                elif k == "created_at" or k == "updated_at":
+                    setattr(self, k, datetime.strptime(v, t_format))
+                else:
+                    setattr(self, k, v)
     def save(self):
         self.updated_at = datetime.utcnow()
     def to_dict(self):
