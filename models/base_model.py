0f75561a82e401ac9fc0b845e2a9baa63f1d30b2
muyenzi10 <muyenzija10@gmail.com>
2024-01-15 03:01:41 -0800

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -18,17 +18,4 @@ class BaseModel:
         return obj_dict
     def __str__(self):
         my_obj = __class__.__name__
-        return "[{}] [{}] [{}]".format(my_obj,self.id,self.__dict__)
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My First Model"
-    my_model.my_number = 89
-    print(my_model)
-    my_model.save()
-    print(my_model)
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
-
+        return "[{}] [{}] [{}]".format(my_obj, self.id, self.__dict__)
