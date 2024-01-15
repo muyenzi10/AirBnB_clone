9df14eec2ab5e4e8339c54ad8f53e1f92061621a
muyenzi10 <muyenzija10@gmail.com>
2024-01-15 02:40:28 -0800

    updated

---
+++ models/base_model.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+from datetime import datetime
+import uuid
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+    def save(self):
+        self.updated_at = datetime.utcnow()
+    def to_dict(self):
+        """
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = __class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+    def __str__(self):
+        my_obj = __class__.__name__
+        return "[{}] [{}] [{}]".format(my_obj,self.id,self.__dict__)
+if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
---
+++ tests/tests_model/test_base_model.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+import unittest
+from models.base_model import BaseModel
+class Test_tools(unittest.TestCase):
+    def __init__(self):
+        """
+        Test initials
+        """
+        my_model = BaseModel()
+
+        self.assertIsNotNone(my_model.id)
+        self.assertIsNotNone(my_model.created_at)
+        self.assertIsNotNone(my_model.updated_at)
+
+    def tes_sav(self):
+        """
+        Test to sav
+        """
+        models = BaseModel()
+
+        initial_updated_at = models.updated_at
+
+        current_updated_at = models.save()
+
+        self.assertNotEqual(initial_updated_at, current_updated_at)
+
+    def o_dict(self):
+        """
+        Test  dict
+        """
+        models = BaseModel()
+
+        my_model_dict = models.to_dict()
+                                  
+        self.assertIsInstance(model_dict, dict)
+                                              
+        self.assertEqual(model_dict["__class__"], 'BaseModel')
+        self.assertEqual(model_dict['id'], models.id)
+        self.assertEqual(model_dict['created_at'], models.created_at.isoformat())
+        self.assertEqual(model_dict["updated_at"], models.created_at.isoformat())
+
+
+    def test_str(self):
+        """
+        Test str
+        """
+        models = BaseModel()
+
+        self.assertTrue(str(models).startswith('[BaseModel]'))
+                       
+        self.assertIn(models.id, str(models))
+                                                  
+        self.assertIn(str(models.__dict__), str(models))
+
+
+if __name__ == "__main__":
+    unittest.main()
